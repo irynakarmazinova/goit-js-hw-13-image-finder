4ff211d18d073874a1559e4c4f82c1fc4dfa2f3f
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-08-20 18:37:59 +0300

    added ponify  error and markup

--- src/index.js
+++ src/index.js
@@ -1,61 +1,130 @@
-import fetchImage from './js/apiService';
+import ImagesApiService from './js/apiService';
+import LoadMoreBtn from './js/load-more-btn';
 
+// handlebars
 import imageCardTpl from './templates/image-card.hbs';
-import loadMoreTpl from './templates/load-more.hbs';
+
+// pnotify
+import { defaultModules, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+
+import '@pnotify/core/dist/BrightTheme.css'; //color theme for error
+
+defaultModules.set(PNotifyMobile, {});
 
 // refs
 const formInputRef = document.getElementById('search-form');
 const galleryListRef = document.getElementById('gallery-list');
-const galleryBoxRef = document.getElementById('gallery-box');
-const element = document.getElementById('my-element-selector'); //нет такого элемента для плавного скрола
+// const loadMoreBtn = document.getElementById('my-element-selector');
 
-function onInput(e) {
-  e.preventDefault();
-
-  const form = e.currentTarget; //сама форма в доме
-  const searchQuery = form.elements.query.value; //именно те буквы, что ввели в форму
-
-  if (form !== '' || e.keyCode !== 32) {
-    fetchImage(searchQuery)
-      .then(data => {
-        renderImage(data);
-
-        // if()
-      })
-      .catch(err => console.error(err.message))
-      .finally(() => formInputRef.reset());
-  }
-}
-formInputRef.addEventListener('submit', onInput);
-
-function renderImage(image) {
-  const markup = imageCardTpl(image);
-  galleryListRef.innerHTML = markup;
-}
-
-function renderLoadMoreBtn() {
-  const markup = loadMoreTpl(image);
-  galleryBoxRef.insertAdjacentHTML('beforeend', markup);
-}
-
-// увеличение страницы
-function incrementPage() {
-  this.page += 1;
-}
-
-// сброс страницы
-function resetPage() {
-  this.page = 1;
-}
-
-// element.scrollIntoView({
+const imagesApiService = new ImagesApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+// loadMoreBtn.refs.button.scrollIntoView({
 //   behavior: 'smooth',
 //   block: 'end',
 // });
+let endPoint = 0;
+
+function setError(data) {
+  const now = new Date().valueOf();
+  console.log(now - endPoint);
+  console.log(now);
+
+  if (now > endPoint) {
+    endPoint = now + data.delay + 1000;
+    return error(data);
+  }
+}
+
+function onSearch(e) {
+  e.preventDefault();
+
+  imagesApiService.query = e.currentTarget.elements.query.value;
+
+  if (!imagesApiService.query.trim()) {
+    //при пробеле идет фетч, почему?
+    // Запрос не должен отправляться при пустой строке ввода (или если просто использует пробел)
+
+    return setError({
+      title: `Please enter some value!`,
+      delay: 500,
+    });
+  }
+
+  // if () {
+  // Кнопку "Load more" не нужно показывать когда картинок меньше или 12, а также когда картинки закончились
+  loadMoreBtn.show();
+  // }
+
+  imagesApiService.resetPage();
+  clearImagesContainer();
+  fetchImagesFn();
+}
+formInputRef.addEventListener('submit', onSearch);
+
+function fetchImagesFn() {
+  loadMoreBtn.disable();
+  imagesApiService.fetchImages().then(hits => {
+    renderImagesMarkup(hits);
+    loadMoreBtn.enable();
+  });
+}
+loadMoreBtn.refs.button.addEventListener('click', fetchImagesFn);
+
+function renderImagesMarkup(hits) {
+  // galleryListRef.insertAdjacentHTML('beforeend', imageCardTpl(hits));
+
+  const markup = hits.reduce((acc, item) => {
+    const el = `<li class='gallery-item'>
+    <div class='photo-card'>
+
+      <div class='gallery-image-box'>
+        <img class='gallery-image' src='${item.webformatURL}' alt='${item.tags}' />
+      </div>
+
+      <div class='stats'>
+
+        <p class='stats-item'>
+          <i class='material-icons'>thumb_up</i>
+          ${item.likes}
+        </p>
+
+        <p class='stats-item'>
+          <i class='material-icons'>visibility</i>
+          ${item.views}
+        </p>
+
+        <p class='stats-item'>
+          <i class='material-icons'>comment</i>
+          ${item.comments}
+        </p>
+
+        <p class='stats-item'>
+          <i class='material-icons'>cloud_download</i>
+          ${item.downloads}
+        </p>
+
+      </div>
+    </div>
+  </li>`;
+
+    return `${acc} ${el}`;
+  }, '');
+
+  galleryListRef.innerHTML = markup;
+}
+
+function clearImagesContainer() {
+  galleryListRef.innerHTML = '';
+}
+
 // --------------------------------------------------------------------------------
 // Для того чтобы корректно работал плавный скролл необходимо зафиксировать высоту карточки.
-// Кнопку "Load more" не нужно показывать когда картинок меньше или 12, а также когда картинки закончились
-// Запрос не должен отправляться при при пустой строке ввода (или если просто использует пробел)
 // --------------------------------------------------------------------------------
 // async function fn () {}
 
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,10 +2,44 @@
 
 import { BASE_URL, API_KEY } from './constants';
 
-export default function fetchImage(search) {
-  return fetch(
-    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12&key=${API_KEY}`
-  ).then(response => {
-    return response.json();
-  });
+// export default function fetchImage(search) {
+//   return fetch(
+//     `${BASE_URL}?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12&key=${API_KEY}`
+//   ).then(response => {
+//     return response.json();
+//   });
+// }
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    return fetch(
+      `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    //   query - хранит сервис, котор отвечает за раб с API, а не внешний код
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
