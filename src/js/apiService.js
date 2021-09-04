f68788eb7cc845997421c8e4359378588f6e10e0
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-09-04 12:48:13 +0300

    commented out import

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,11 +1,10 @@
 // файл apiService.js с дефолтным экспортом объекта отвечающего за логику HTTP-запросов к API
 
-import { BASE_URL, API_KEY } from './constants';
-
 import axios from 'axios';
 axios.defaults.baseURL =
   'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
 
+// import { BASE_URL, API_KEY } from './constants';
 // export default function fetchImage(search) {
 //   return fetch(
 //     `${BASE_URL}?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12&key=${API_KEY}`
